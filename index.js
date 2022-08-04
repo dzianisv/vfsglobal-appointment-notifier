c1e8fd3fcb4eed31c50477fc94b39ad7d8efe2d0
Anonymous <2119348+dzianisv@users.noreply.github.com>
2022-08-04 00:17:28 -0700

    Add indefinite loop

--- index.js
+++ index.js
@@ -1,6 +1,59 @@
 const puppeteer = require('puppeteer');
 const config = require("./config.json");
 
+
+async function go(page) {
+  await page.goto('https://visa.vfsglobal.com/can/en/nld/dashboard');
+  await page.waitForNavigation();
+
+  // Login Page
+  try {
+    const buttonXPath = "//button/span[contains(text(), 'Sign In')]";
+    await page.waitForXPath(buttonXPath, {timeout: 5000});
+
+    const signInButtonXPath = await page.$x(buttonXPath);
+
+    await page.waitForSelector("#mat-input-0");
+    await page.type("#mat-input-0", config.username);
+
+    await page.waitForSelector("#mat-input-1");
+    await page.type("#mat-input-1", config.password);
+
+    await signInButtonXPath[0].click();
+  } catch (err) {
+    console.log("No Sign In Page", err);
+  }
+
+  // Dashboard Page
+  console.log("Start New Booking");
+  {
+    const xpath = "//button/span[contains(text(), ' Start New Booking ')]";
+    await page.waitForXPath(xpath);
+    let objects = await page.$x(xpath);
+    await objects[1].click();
+  }
+
+  console.log("Select Vancouver Apllication center");
+  {
+    await page.click("#mat-select-0");
+    const xpath = "//span[contains(text(), 'Netherlands-Canada-Visa Application center - Vancouver')]";
+    await page.waitForXPath(xpath);
+    await (await page.$x(xpath))[1].click();
+  }
+
+  console.log("Select Short-stay Schenge visa");
+  {
+    await page.click("#mat-select-4");
+    const xpath = "//span[contains(text(), 'Short-stay Schengen visa')]";
+    await page.waitForXPath(xpath);
+    await (await page.$x(xpath))[1].click();
+  }
+
+  await page.waitForNavigation();
+  const text = await page.evaluate(() => document.querySelector('div.alert').innerText);
+  return text;
+}
+
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
@@ -8,42 +61,16 @@ const config = require("./config.json");
     userDataDir: ".data"
   });
   const page = await browser.newPage();
-  await page.goto('https://visa.vfsglobal.com/can/en/nld/dashboard');
-  await page.waitForNavigation();
 
-  // Login Page
-  const signInButtonXPath = "//button/span[contains(text(), 'Sign In')]";
-  if ((await page.$x(signInButtonXPath)).length > 0) {
-    await page.waitForSelector("#mat-input-0");
-    await page.type("#mat-input-0", config.username);
-
-    await page.waitForSelector("#mat-input-1");
-    await page.type("#mat-input-1", config.password);
-
-    await page.$x(signInButtonXPath).click();
+  while (true) {
+    try {
+      await go(page);
+    } catch(e) {
+      console.error(e);
+    }
+    console.log("Wait 1h");
+    page.waitForTimeout(3600 * 1000);
   }
-
-  // Dashboard Page
-  const bookButtonXPath = "//button/span[contains(text(), 'Start New Booking')]";
-  await page.waitForXPath(bookButtonXPath);
-  await page.waitForTimeout(10000);
-  let buttons = await page.$x(bookButtonXPath);
-  await buttons[0].click();
-
-  await page.waitForNavigation();
-
-  //Apoitment Details Page
-  await page.click("#mat-select-0");
-  await page.click("#mat-option-2"); // Vancouver
-
-  await page.click("#mat-select-4");
-  await page.click("#mat-option-5"); // Short-Term Visa
-
-  await page.waitForNavigation();
-  let alerts = await page.$("div.alert");
-  let alertText = alerts[0].innerText;
-
-  console.log(alertText);
 
   await browser.close();
 })();
