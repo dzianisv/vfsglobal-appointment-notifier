765d4ce19edd9772d3dbb9dac7a85957207f02a3
Anonymous <2119348+dzianisv@users.noreply.github.com>
2022-08-25 18:19:03 -0700

    Update index.js

--- index.js
+++ index.js
@@ -62,15 +62,62 @@ async function go(page) {
   });
   const page = await browser.newPage();
 
-  while (true) {
-    try {
-      await go(page);
-    } catch(e) {
-      console.error(e);
-    }
-    console.log("Wait 1h");
-    page.waitForTimeout(3600 * 1000);
+  await page.goto('https://visa.vfsglobal.com/can/en/nld/dashboard');
+  await page.waitForNavigation();
+
+  // Login Page
+  try {
+    const buttonXPath = "//button/span[contains(text(), 'Sign In')]";
+    await page.waitForXPath(buttonXPath, {timeout: 5000});
+
+    const signInButtonXPath = await page.$x(buttonXPath);
+
+    await page.waitForSelector("#mat-input-0");
+    await page.type("#mat-input-0", config.username);
+
+    await page.waitForSelector("#mat-input-1");
+    await page.type("#mat-input-1", config.password);
+
+    await signInButtonXPath[0].click();
+  } catch (err) {
+    console.log("No Sign In Page", err);
   }
 
+  // Dashboard Page
+  console.log(" Start New Booking ");
+  {
+    await page.waitForTimeout(5000);
+    const xpath = "//button/span[contains(text(), ' Start New Booking ')]";
+    await page.waitForXPath(xpath);
+    let objects = await page.$x(xpath);
+    console.log(objects);
+    await objects[1].click();
+  }
+
+  console.log("Select Vancouver Apllication center");
+  {
+    await page.waitForSelector("#mat-select-0")
+    await page.click("#mat-select-0");
+    const xpath = "//span[contains(text(), 'Netherlands-Canada-Visa Application center - Vancouver')]";
+    await page.waitForXPath(xpath);
+    const objects = await page.$x(xpath);
+    console.log(objects);
+    objects[0].click();
+  }
+
+  console.log("Select Short-stay Schengen visa");
+  {
+    await page.waitForSelector("#mat-select-4");
+    await page.click("#mat-select-4");
+    const xpath = "//span[contains(text(), ' Short-stay Schengen visa ')]";
+    await page.waitForXPath(xpath);
+    const objects = await page.$x(xpath);
+    console.log(objects);
+    objects[0].click();
+  }
+
+  await page.waitForNavigation();
+  const text = await page.evaluate(() => document.querySelector('div.alert').innerText);
+  console.log(text);
   await browser.close();
 })();
